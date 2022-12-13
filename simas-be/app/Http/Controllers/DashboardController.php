75c9133de36b9e8f837c2ece3f1654c1b227a317
lamkhil <43593728+lamkhil@users.noreply.github.com>
2022-12-13 15:00:31 +0700

    Update DashboardController.php

--- simas-be/app/Http/Controllers/DashboardController.php
+++ simas-be/app/Http/Controllers/DashboardController.php
@@ -16,48 +16,91 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        return response(['message' => "OK"]);
+        $time = WaktuSampling::orderBy('waktu', 'DESC')->get();
+        $ika = KualitasAir::join('waktu_samplings', 'kualitas_airs.waktu_sampling_id', '=', 'waktu_samplings.id')
+                ->where('waktu_samplings.waktu', $time[0]['waktu'])
+                ->avg('ika');
+        $ika_before = 0;
+        if ($time->count()>1) {
+            $ika_before = KualitasAir::join('waktu_samplings', 'kualitas_airs.waktu_sampling_id', '=', 'waktu_samplings.id')
+                ->where('waktu_samplings.waktu', $time[1]['waktu'])
+                ->avg('ika');
+        }
+        $ika_comparison = $ika > $ika_before ? 'Naik' : 'Turun';
+        $min_ika = KualitasAir::join('waktu_samplings', 'kualitas_airs.waktu_sampling_id', '=', 'waktu_samplings.id')
+                ->where('waktu_samplings.waktu', $time[0]['waktu'])->min('ika');
+               
+        $ika_terendah = KualitasAir::where('ika','=',$min_ika)->where('waktu_sampling_id','=',$time[0]['id'])->first()->titik_pantau;
+        $ika_terendah['ika'] = $min_ika;
+        $ika_terendah_before = 0;
+        if ($time->count()>1) {
+            $ika_terendah_before = KualitasAir::join('waktu_samplings', 'kualitas_airs.waktu_sampling_id', '=', 'waktu_samplings.id')
+            ->where('waktu_samplings.waktu', $time[1]['waktu'])->min('ika');
+        }
+        $ika_terendah_comparison = $min_ika > $ika_terendah_before?'Naik':"Turun";
+        $ika_terendah['ika_compare']=$ika_terendah_comparison;
+        return response(
+            [
+                "message" => "Success",
+                "status" => true,
+                "data" => [
+                    "ika"=>$ika,
+                    "ika_compare"=>$ika_comparison,
+                    "ketinggan_air"=>null,
+                    "ika_terendah"=>$ika_terendah,
+                    "ketinggian_compare"=>null,
+                    "tingkat_kecemaran"=>$this->status($ika)
+                ]
+            ]
+        );
+
     }
 
     public function upper()
     {
         $time = WaktuSampling::select('waktu')->orderBy('waktu', 'DESC')->get();
-
-        if ($time->count() > 0) {
-            $ika = KualitasAir::join('waktu_sampling', 'kualitas_airs.waktu_sampling_id', '=', 'waktu_samplings.id')
+        $ika = KualitasAir::join('waktu_sampling', 'kualitas_airs.waktu_sampling_id', '=', 'waktu_samplings.id')
                 ->where('waktu_samplings.waktu', $time[0]['waktu'])
                 ->avg('ika');
-
+        $ika_before = 0;
+        if ($time->count()>1) {
             $ika_before = KualitasAir::join('waktu_sampling', 'kualitas_airs.waktu_sampling_id', '=', 'waktu_samplings.id')
                 ->where('waktu_samplings.waktu', $time[1]['waktu'])
                 ->avg('ika');
-
-            $ika_comparison = $ika > $ika_before ? 'Naik' : 'Turun';
-
-            $min_ika = KualitasAir::join('waktu_sampling', 'kualitas_airs.waktu_sampling_id', '=', 'waktu_samplings.id')
-                ->where('waktu_samplings.waktu', $time[1]['waktu'])->min('ika');
-
-            return response($min_ika);
         }
+        $ika_comparison = $ika > $ika_before ? 'Naik' : 'Turun';
+        $min_ika = KualitasAir::join('waktu_sampling', 'kualitas_airs.waktu_sampling_id', '=', 'waktu_samplings.id')
+                ->where('waktu_samplings.waktu', $time[0]['waktu'])->min('ika');
 
         return response(
             [
-                "message" => "Belum ada data",
+                "message" => "Success",
                 "status" => true,
                 "data" => [
-                    "ika"=>null,
-                    "ika_comparison"=>null,
+                    "ika"=>$ika,
+                    "ika_comparison"=>$ika_comparison,
                     "ketinggan_air"=>null,
-                    "ika_terendah"=>[
-                        "titik_pantau"=>null,
-                        "ika"=>null,
-                        "ika_comparison"=>null
-                    ],
+                    "ika_terendah"=>$min_ika,
                     "ketinggian_comparison"=>null,
-                    "tingkat_kecemaran"=>null
+                    "tingkat_kecemaran"=>$this->status($ika)
                 ]
             ]
         );
+    }
+
+    private function status($ika)
+    {
+        if ($ika > 90) {
+            return "Sangat Bersih";
+        } elseif ($ika > 70) {
+            return "Bersih";
+        } elseif ($ika > 50) {
+            return "Cemar Ringan";
+        } elseif ($ika > 25) {
+            return "Cemar Berat";
+        } else {
+            return "Sangat Tercemar";
+        }
     }
 
     public function titikPantau(){
